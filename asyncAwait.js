a88b031a2324bf9c7c86d56e5bcec63fc6a1460f
SANJAI SABARI <sanjays18473@gmail.com>
2024-12-29 13:33:28 +0530

    async commit

---
+++ asyncAwait.js
@@ -0,0 +1,23 @@
+/*async function name() {
+    return await "hii"
+}
+
+async function name1() {
+    return await"hello worls"
+    
+}
+
+console.log(name())
+console.log(name1())
+*/
+async function name() {
+   console.log(await "hii"),
+}
+
+async function name1() {
+    console.log(await "hello")
+    
+}
+
+console.log(name())
+console.log(name1())
